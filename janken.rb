9c325bb19dfc43746f2674760f1f1e83b58b0984
takashi otsuka <shoshi100010000@gmail.com>
2022-07-07 13:57:31 +0900

    fix janken

--- janken.rb
+++ janken.rb
@@ -6,24 +6,29 @@ class Player
     puts "0:グー, 1:チョキ, 2:パー"
     # 変数「input_hand」にプレイヤーの入力値を代入します。
     # ヒント：getsメソッドについて調べてみましょう。
-    input_hand = gets.to_i
+
     # 「input_hand」が「0, 1, 2」のいずれかだと繰り返し処理を終了し、それ以外（アルファベットも含む）だと繰り返し処理を継続します。
     while true
       # if 「input_hand」が「0, 1, 2」のいずれかの場合だった場合
       # ヒント：include?メソッドについて調べてみましょう。
-      hands = [0,1,2]
+      input_hand = gets.chomp
+      hands = ["0","1","2"]
         # 「input_hand」をそのまま返す。
         # ヒント：戻り値を返して繰り返し処理を終了させたい場合、「return」を使用します。
-      return input_hand if hands.include?(input_hand)
+        if hands.include?(input_hand)
+          break
+        end
 
       # else それ以外の場合
         # プレイヤーに「0〜2」を入力させる文章を表示させる。
       puts "0〜2の数字を入力してください。"
       puts "0:グー, 1:チョキ, 2:パー"
         # 変数「input_hand」にプレイヤーの入力値を代入します。
-      input_hand = gets.to_i
+
       # end if文のend
+
     end
+    input_hand = input_hand.to_i
   end
 end
 # 相手が「0~2」の値をランダムに生成するロジックを書きます。
